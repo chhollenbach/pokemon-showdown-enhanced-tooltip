01d894cccfef66c48a02036882e7c13420b7f1b8
chhollenbach <hollenbach256@gmail.com>
2021-09-18 19:42:28 -0500

    extension made, but issues with incorporation damange calc. need to figure out webpack

---
+++ src/enhanced-tooltip.js
@@ -0,0 +1,24 @@
+let test = 'Hello World';
+console.log(`${test}`);
+
+
+// testing smogon damage calc
+// import {calculate, Generations, Pokemon, Move} from '@smogon/calc';
+
+// const gen = Generations.get(8); // alternatively: const gen = 8;
+// const result = calculate(
+//   gen,
+//   new Pokemon(gen, 'Gengar', {
+//     item: 'Choice Specs',
+//     nature: 'Timid',
+//     evs: {spa: 252},
+//     boosts: {spa: 1},
+//   }),
+//   new Pokemon(gen, 'Chansey', {
+//     item: 'Eviolite',
+//     nature: 'Calm',
+//     evs: {hp: 252, spd: 252},
+//   }),
+//   new Move(gen, 'Focus Blast')
+// );
+// console.log(result)
