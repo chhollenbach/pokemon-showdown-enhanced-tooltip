60be5daab9ebf08d08b6bf97902a0a9fab6f71eb
chhollenbach <hollenbach256@gmail.com>
2021-09-19 21:42:53 -0500

    woohoo! webpack was the simple solution after all. Static damage calc performed upon loading pokemon showdown

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    enhancement_suite: './src/enhancement-suite.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dest/')
+  }
+};
